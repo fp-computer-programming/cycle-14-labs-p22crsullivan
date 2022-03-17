dfb632e390d8e3008e222d0ae95aeda8c8fa8b5e
p22crsullivan <p22crsullivan@fairfieldprep.org>
2022-03-17 09:48:36 -0400

    Submit Cycle 14 Labs

---
+++ lab_14-1.py
@@ -0,0 +1,18 @@
+# Author: CRS 03/17/22
+import turtle
+
+
+window = turtle.Screen()
+window.setup(1000, 1000)
+window.title("Lab 1")
+steve = turtle.Turtle()
+steve.right(90)
+steve.forward(250)
+steve.left(90)
+steve.forward(100)
+steve.left(90)
+steve.forward(250)
+steve.left(90)
+steve.forward(100)
+
+window.mainloop()
---
+++ lab_14-2.py
@@ -0,0 +1,15 @@
+# Author: CRS 03/17/22
+import turtle
+
+
+window = turtle.Screen()
+window.setup(500, 500)
+window.title("Lab 2")
+joseph = turtle.Turtle()
+joseph.forward(100)
+joseph.right(120)
+joseph.forward(100)
+joseph.right(120)
+joseph.forward(100)
+
+window.mainloop()
