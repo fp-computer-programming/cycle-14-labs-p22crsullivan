2bf44ab349dc74a6cd973be5be4087f8d403fd8f
p22crsullivan <p22crsullivan@fairfieldprep.org>
2022-03-17 09:51:22 -0400

    Submit Cycle 14 Labs

--- lab_14-1.py
+++ lab_14-1.py
@@ -1,11 +1,13 @@
 # Author: CRS 03/17/22
+# Import turtle
 import turtle
 
-
+# Create the window
 window = turtle.Screen()
 window.setup(1000, 1000)
 window.title("Lab 1")
 steve = turtle.Turtle()
+# Direct the turtle
 steve.right(90)
 steve.forward(250)
 steve.left(90)
@@ -14,5 +16,5 @@ steve.left(90)
 steve.forward(250)
 steve.left(90)
 steve.forward(100)
-
+# Make sure the window doesn't close
 window.mainloop()
--- lab_14-2.py
+++ lab_14-2.py
@@ -1,15 +1,17 @@
 # Author: CRS 03/17/22
+# Import turtle
 import turtle
 
-
+# Create the window
 window = turtle.Screen()
 window.setup(500, 500)
 window.title("Lab 2")
 joseph = turtle.Turtle()
+# Direct the turtle
 joseph.forward(100)
 joseph.right(120)
 joseph.forward(100)
 joseph.right(120)
 joseph.forward(100)
-
+# Make sure the window doesn't close
 window.mainloop()
